179172c88a293356797e282ca5c80d1de682308f
fabiano1749 <fabiano1749@gmail.com>
2020-08-28 12:05:07 -0300

    Alteração no arquivo de migração

--- orcaoline-api/src/main/resources/db/migration/V001__Criacao_tabela_Produto.sql
+++ orcaoline-api/src/main/resources/db/migration/V001__Criacao_tabela_Produto.sql
@@ -1,9 +1,47 @@
-CREATE TABLE produto(
-	id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
-	codigo varchar(13),
-	nome varchar(150),
-	descricao varchar(200),
-	familia varchar(100),
-	segmento varchar(100),
-	classe varchar(100)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `segmento` (
+  `id` bigint PRIMARY KEY AUTO_INCREMENT,
+  `nome` varchar(150) DEFAULT NULL,
+  `descricao` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `familia` (
+  `id` bigint PRIMARY KEY AUTO_INCREMENT,
+  `nome` varchar(150) DEFAULT NULL,
+  `descricao` varchar(200) DEFAULT NULL,
+  `id_segmento` bigint DEFAULT NULL,
+  CONSTRAINT `fk_familia_idSegmento` FOREIGN KEY (`id_segmento`) REFERENCES `segmento` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `classe` (
+  `id` bigint PRIMARY KEY AUTO_INCREMENT,
+  `nome` varchar(150) DEFAULT NULL,
+  `descricao` varchar(200) DEFAULT NULL,
+  `id_familia` bigint DEFAULT NULL,
+  CONSTRAINT `fk_classe_idFamilia` FOREIGN KEY (`id_familia`) REFERENCES `familia` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `brick` (
+  `id` bigint PRIMARY KEY AUTO_INCREMENT,
+  `nome` varchar(150) DEFAULT NULL,
+  `descricao` varchar(200) DEFAULT NULL,
+  `id_classe` bigint DEFAULT NULL,
+  CONSTRAINT `fk_brick_idClasse` FOREIGN KEY (`id_classe`) REFERENCES `classe` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `gtin_ean` (
+  `id` bigint PRIMARY KEY AUTO_INCREMENT,
+  `numero` bigint DEFAULT NULL,
+  `id_brick` bigint DEFAULT NULL,
+  CONSTRAINT `fk_gtin_ean_idBrick` FOREIGN KEY (`id_brick`) REFERENCES `brick` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `produto` (
+  `id` bigint PRIMARY KEY AUTO_INCREMENT,
+  `nome` varchar(150) DEFAULT NULL,
+  `descricao` varchar(200) DEFAULT NULL,
+  `ncm` varchar(8) DEFAULT NULL,
+  `id_gtin_ean` bigint DEFAULT NULL,
+  CONSTRAINT `fk_produto_idGTIN` FOREIGN KEY (`id_gtin_ean`) REFERENCES `gtin_ean` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+
