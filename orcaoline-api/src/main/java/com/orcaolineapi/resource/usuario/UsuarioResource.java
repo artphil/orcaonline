fa5c714d38ccf572ff8a2c09eb1b3dae92d38405
fabiano1749 <fabiano1749@gmail.com>
2020-09-30 16:00:05 -0300

    Alterações ao salvar usuário na api

--- orcaoline-api/src/main/java/com/orcaolineapi/resource/usuario/UsuarioResource.java
+++ orcaoline-api/src/main/java/com/orcaolineapi/resource/usuario/UsuarioResource.java
@@ -1,9 +1,18 @@
 package com.orcaolineapi.resource.usuario;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.orcaolineapi.event.RecursoCriadoEvent;
 import com.orcaolineapi.modelo.usuario.Usuario;
 import com.orcaolineapi.repository.AbstractRepository;
 import com.orcaolineapi.repository.usuario.UsuarioRepository;
@@ -19,6 +28,14 @@ public class UsuarioResource extends AbstractResource<Usuario> {
 
 	private @Autowired UsuarioService service;
 
+	@CrossOrigin
+	@PostMapping
+	public ResponseEntity<Usuario> save(@Valid @RequestBody Usuario resource, HttpServletResponse response) {
+		Usuario resourceSave = service.save(resource);
+		getPublisher().publishEvent(new RecursoCriadoEvent(this, response, resourceSave.getId()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(resourceSave);
+	}
+	
 	@Override
 	public AbstractRepository<Usuario, Long> getRepository() {
 		return repository;
