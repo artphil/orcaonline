b47414c3d215672add13264fe23410b863819d13
fabiano1749 <fabiano1749@gmail.com>
2020-10-14 11:19:15 -0300

    Alterações na criação do orçamento

---
+++ orcaoline-api/src/main/resources/db/migration/V021__Altera_Tabela_Item_Orcamento.sql
@@ -0,0 +1,6 @@
+ALTER TABLE `orcaoline`.`item_orcamento` 
+CHANGE COLUMN `valor_unitario` `valor_unitario` DOUBLE NULL ;
+ALTER TABLE `orcaoline`.`item_orcamento` 
+CHANGE COLUMN `id_produto` `id_produto` BIGINT NULL ;
+
+
