9266453f575a1eea195f3f1020c6ec13e04c61be
aikaterhine <catarina.edp@gmail.com>
2020-11-01 17:31:26 -0300

    atualizando testes de resources de status

--- orcaoline-api/src/test/java/com/orcaolineapi/resource/orcamento/StatusResourceTest.java
+++ orcaoline-api/src/test/java/com/orcaolineapi/resource/orcamento/StatusResourceTest.java
@@ -2,20 +2,34 @@ package com.orcaolineapi.resource.orcamento;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.standaloneSetup;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.exceptions.verification.opentest4j.ArgumentsAreDifferent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.util.NestedServletException;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.orcaolineapi.modelo.orcamento.Status;
 import com.orcaolineapi.repository.orcamento.StatusRepository;
 import com.orcaolineapi.service.orcamento.StatusService;
@@ -40,25 +54,157 @@ class StatusResourceTest {
 	}
 
 	// nomeDoMetodo / resultadoEsperado / emQueSituação
+
+	@Test
+	public void putRecursos_Sucesso_InserirRecursosExistentes() throws Exception {
+						
+		Status sta1 = new Status(1L);
+		Status sta2 = new Status(2L);
+		
+		when(this.repository.save(sta1)).thenReturn(sta1);
+		
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		String json = ow.writeValueAsString(sta2);
+				
+		json = json.replaceAll("\"" + "id" + "\"[ ]*:[^,}\\]]*[,]?", "");
+				
+		System.out.println(json);
+		
+		given().body(json).contentType(ContentType.JSON).when().put("/status/{id}", sta1.getId()).andReturn().then().statusCode(HttpStatus.OK.value());
+			
+	}
+	
+	@Test
+	public void postRecursos_Sucesso_InserirRecursosExistentes() throws Exception {
+						
+		Status sta1 = new Status(1L);
+		
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		String json = ow.writeValueAsString(sta1);
+				
+		json = json.replaceAll("\"" + "id" + "\"[ ]*:[^,}\\]]*[,]?", "");
+				
+		System.out.println(json);
+		
+		given().body(json).contentType(ContentType.JSON).when().post("/status").andReturn().then().statusCode(HttpStatus.OK.value());
+			
+	}
+	
+	@Test
+	public void getRecursos_Sucesso_BuscarRecursosExistentes() throws Exception {
+
+		Status sta1 = new Status(1L);
+		Status sta2 = new Status(2L);
+		Status sta3 = new Status(3L);
+		Status sta4 = new Status(4L);
+		
+		sta1.setId(Long.valueOf(1));
+		sta2.setId(Long.valueOf(2));
+		sta3.setId(Long.valueOf(3));
+		sta4.setId(Long.valueOf(4));
+		
+		when(this.repository.findAll()).thenReturn(Arrays.asList(sta1, sta2, sta3, sta4));
+		
+		given().accept(ContentType.JSON).when().get("/status").andReturn().then().statusCode(HttpStatus.OK.value())
+	    .expect(jsonPath("$[*]", hasSize(4)))
+	    .expect(jsonPath("$[0].id", is(1)))
+	    .expect(jsonPath("$[0].nome", is("ABERTO")))
+	    .expect(jsonPath("$[1].id", is(2)))
+	    .expect(jsonPath("$[1].nome", is("ATIVO")))
+	    .expect(jsonPath("$[2].id", is(3)))
+	    .expect(jsonPath("$[2].nome", is("FECHADO")))
+	    .expect(jsonPath("$[3].id", is(4)))
+	    .expect(jsonPath("$[3].nome", is("CANCELADO")));
+		
+		verify(repository, times(1)).findAll();
+        verifyNoMoreInteractions(repository);
+   }
+	
+	@Test
+	public void getRecursos_EmptyList_BuscarRecursosInexistentes() {
+		
+		when(this.repository.findAll()).thenReturn(Collections.emptyList());
+
+		given().accept(ContentType.JSON).when().get("/status").then().statusCode(HttpStatus.OK.value())
+		.expect(jsonPath("$[*]", hasSize(0)));
+		
+		verify(repository, times(1)).findAll();
+        verifyNoMoreInteractions(repository);
+
+	}
+	
 	@Test()
 	public void getById_Sucesso_BuscarUmRecursoExistente() {
 
-		Status sta = new Status(1L);
+		Status sta1 = new Status(1L);
+		sta1.setId(1L);
+
+		when(this.repository.findById(1L)).thenReturn(Optional.of(sta1));
+
+		given().accept(ContentType.JSON).when().get("/status/{id}", 1L).then().statusCode(HttpStatus.OK.value())
+		.expect(jsonPath("$.id", is(1)))
+	    .expect(jsonPath("$.nome", is("ABERTO")));
 		
-		sta.setId(1L);
-
-		when(this.repository.findById(1L)).thenReturn(Optional.of(sta));
-
-		given().accept(ContentType.JSON).when().get("/status/{id}", 1L).then().statusCode(HttpStatus.OK.value());
+		verify(repository, times(1)).findById(1L);
+        verifyNoMoreInteractions(repository);
 
 	}
 
-//	@Test
-	public void getById_Falha_BuscarUmRecursoInexistente() {
-		when(this.repository.findById(1L)).thenReturn(null);
+	@Test
+	public void getById_NestedServletException_BuscarUmRecursoInexistente() {
+        
+		Throwable exception = assertThrows(NestedServletException.class, () -> {
+			when(this.repository.findById(140L)).thenReturn(null);
 
-		given().accept(ContentType.JSON).when().get("/status/{id}", 140L).then()
-				.statusCode(HttpStatus.NOT_FOUND.value());
+			given().accept(ContentType.JSON).when().get("/status/{id}", 140L).then()
+					.statusCode(HttpStatus.NOT_FOUND.value());
+			
+			verify(repository, times(1)).findById(1L);
+	        verifyNoMoreInteractions(repository);
+		});
+        
+	}
+	
+	@Test
+	public void deleteById_NestedServletException_BuscarUmRecursoInexistente() {
+        
+		Throwable exception = assertThrows(ArgumentsAreDifferent.class, () -> {
+			Status sta1 = new Status(1L);
+			sta1.setId(1L);
+			
+			when(this.repository.save(sta1)).thenReturn(sta1);
+			
+			ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+			String json = ow.writeValueAsString(sta1);
+					
+			json = json.replaceAll("\"" + "id" + "\"[ ]*:[^,}\\]]*[,]?", "");
+							
+			given().contentType(ContentType.JSON).when().delete("/status/{id}", 140L).andReturn().then().statusCode(HttpStatus.NO_CONTENT.value());
+				
+			verify(repository, times(1)).deleteById(1L);
+		    verifyNoMoreInteractions(repository);        
+		});
+        
+	}
+	
+	@Test
+	public void deleteById_Sucesso_BuscarUmRecursoExistente() throws JsonProcessingException {
+        			
+		Status sta1 = new Status(1L);
+		sta1.setId(1L);
+		
+		when(this.repository.save(sta1)).thenReturn(sta1);
+		
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		String json = ow.writeValueAsString(sta1);
+				
+		json = json.replaceAll("\"" + "id" + "\"[ ]*:[^,}\\]]*[,]?", "");
+						
+		given().contentType(ContentType.JSON).when().delete("/status/{id}", 1L).andReturn().then().statusCode(HttpStatus.NO_CONTENT.value());
+			
+		verify(repository, times(1)).deleteById(1L);
+	    verifyNoMoreInteractions(repository);        
 	}
 
+	
 }
