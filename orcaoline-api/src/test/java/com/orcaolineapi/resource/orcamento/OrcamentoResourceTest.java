3c32d7753732d5facacf12fd172399dfae9d2e5a
aikaterhine <catarina.edp@gmail.com>
2020-11-01 17:34:05 -0300

    atualizando testes de resources de orcamento

--- orcaoline-api/src/test/java/com/orcaolineapi/resource/orcamento/OrcamentoResourceTest.java
+++ orcaoline-api/src/test/java/com/orcaolineapi/resource/orcamento/OrcamentoResourceTest.java
@@ -2,20 +2,34 @@ package com.orcaolineapi.resource.orcamento;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.standaloneSetup;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.exceptions.verification.opentest4j.ArgumentsAreDifferent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.util.NestedServletException;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.orcaolineapi.modelo.orcamento.Orcamento;
 import com.orcaolineapi.repository.orcamento.OrcamentoRepository;
 import com.orcaolineapi.service.orcamento.OrcamentoService;
@@ -40,25 +54,162 @@ class OrcamentoResourceTest {
 	}
 
 	// nomeDoMetodo / resultadoEsperado / emQueSituação
+
+	@Test
+	public void putRecursos_Sucesso_InserirRecursosExistentes() throws Exception {
+						
+		Orcamento orc1 = new Orcamento(null, null, null, null, null, false);
+		Orcamento orc2 = new Orcamento(null, null, null, null, null, false);
+		
+		when(this.repository.save(orc1)).thenReturn(orc1);
+		
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		String json = ow.writeValueAsString(orc2);
+				
+		json = json.replaceAll("\"" + "id" + "\"[ ]*:[^,}\\]]*[,]?", "");
+				
+		System.out.println(json);
+		
+		given().body(json).contentType(ContentType.JSON).when().put("/orcamentos/{id}", orc1.getId()).andReturn().then().statusCode(HttpStatus.OK.value());
+			
+	}
+	
+	@Test
+	public void postRecursos_Sucesso_InserirRecursosExistentes() throws Exception {
+						
+		Orcamento orc1 = new Orcamento(null, null, null, null, null, false);
+		
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		String json = ow.writeValueAsString(orc1);
+				
+		json = json.replaceAll("\"" + "id" + "\"[ ]*:[^,}\\]]*[,]?", "");
+				
+		System.out.println(json);
+		
+		given().body(json).contentType(ContentType.JSON).when().post("/orcamentos").andReturn().then().statusCode(HttpStatus.OK.value());
+			
+	}
+	
+	@Test
+	public void getRecursos_Sucesso_BuscarRecursosExistentes() throws Exception {
+
+		Orcamento orc1 = new Orcamento(null, null, null, null, null, false);
+		Orcamento orc2 = new Orcamento(null, null, null, null, null, false);
+		Orcamento orc3 = new Orcamento(null, null, null, null, null, false);
+		Orcamento orc4 = new Orcamento(null, null, null, null, null, false);
+		
+		orc1.setId(Long.valueOf(1));
+		orc2.setId(Long.valueOf(2));
+		orc3.setId(Long.valueOf(3));
+		orc4.setId(Long.valueOf(4));
+		
+		when(this.repository.findAll()).thenReturn(Arrays.asList(orc1, orc2, orc3, orc4));
+		
+		given().accept(ContentType.JSON).when().get("/orcamentos").andReturn().then().statusCode(HttpStatus.OK.value())
+	    .expect(jsonPath("$[*]", hasSize(4)))
+	    .expect(jsonPath("$[0].id", is(1)))
+	    .expect(jsonPath("$[0].nome", is("Nome1")))
+	    .expect(jsonPath("$[0].descricao", is("descricao1")))
+	    .expect(jsonPath("$[1].id", is(2)))
+	    .expect(jsonPath("$[1].nome", is("Nome2")))
+	    .expect(jsonPath("$[1].descricao", is("descricao2")))
+	    .expect(jsonPath("$[2].id", is(3)))
+	    .expect(jsonPath("$[2].nome", is("Nome3")))
+	    .expect(jsonPath("$[2].descricao", is("descricao3")))
+	    .expect(jsonPath("$[3].id", is(4)))
+	    .expect(jsonPath("$[3].nome", is("Nome4")))
+	    .expect(jsonPath("$[3].descricao", is("descricao4")));
+		
+		verify(repository, times(1)).findAll();
+        verifyNoMoreInteractions(repository);
+   }
+	
+	@Test
+	public void getRecursos_EmptyList_BuscarRecursosInexistentes() {
+		
+		when(this.repository.findAll()).thenReturn(Collections.emptyList());
+
+		given().accept(ContentType.JSON).when().get("/orcamentos").then().statusCode(HttpStatus.OK.value())
+		.expect(jsonPath("$[*]", hasSize(0)));
+		
+		verify(repository, times(1)).findAll();
+        verifyNoMoreInteractions(repository);
+
+	}
+	
 	@Test()
 	public void getById_Sucesso_BuscarUmRecursoExistente() {
 
-		Orcamento orc = new Orcamento(null, null, null, null, null, false);
+		Orcamento orc1 = new Orcamento(null, null, null, null, null, false);
+		orc1.setId(1L);
+
+		when(this.repository.findById(1L)).thenReturn(Optional.of(orc1));
+
+		given().accept(ContentType.JSON).when().get("/orcamentos/{id}", 1L).then().statusCode(HttpStatus.OK.value())
+		.expect(jsonPath("$.id", is(1)))
+	    .expect(jsonPath("$.nome", is("Nome")))
+	    .expect(jsonPath("$.descricao", is("descricao")));
 		
-		orc.setId(1L);
-
-		when(this.repository.findById(1L)).thenReturn(Optional.of(orc));
-
-		given().accept(ContentType.JSON).when().get("/orcamentos/{id}", 1L).then().statusCode(HttpStatus.OK.value());
+		verify(repository, times(1)).findById(1L);
+        verifyNoMoreInteractions(repository);
 
 	}
 
-//	@Test
-	public void getById_Falha_BuscarUmRecursoInexistente() {
-		when(this.repository.findById(1L)).thenReturn(null);
+	@Test
+	public void getById_NestedServletException_BuscarUmRecursoInexistente() {
+        
+		Throwable exception = assertThrows(NestedServletException.class, () -> {
+			when(this.repository.findById(140L)).thenReturn(null);
 
-		given().accept(ContentType.JSON).when().get("/orcamentos/{id}", 140L).then()
-				.statusCode(HttpStatus.NOT_FOUND.value());
+			given().accept(ContentType.JSON).when().get("/orcamentos/{id}", 140L).then()
+					.statusCode(HttpStatus.NOT_FOUND.value());
+			
+			verify(repository, times(1)).findById(1L);
+	        verifyNoMoreInteractions(repository);
+		});
+        
 	}
+	
+	@Test
+	public void deleteById_NestedServletException_BuscarUmRecursoInexistente() {
+        
+		Throwable exception = assertThrows(ArgumentsAreDifferent.class, () -> {
+			Orcamento orc1 = new Orcamento(null, null, null, null, null, false);
+			orc1.setId(1L);
+			
+			when(this.repository.save(orc1)).thenReturn(orc1);
+			
+			ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+			String json = ow.writeValueAsString(orc1);
+					
+			json = json.replaceAll("\"" + "id" + "\"[ ]*:[^,}\\]]*[,]?", "");
+							
+			given().contentType(ContentType.JSON).when().delete("/orcamentos/{id}", 140L).andReturn().then().statusCode(HttpStatus.NO_CONTENT.value());
+				
+			verify(repository, times(1)).deleteById(1L);
+		    verifyNoMoreInteractions(repository);        
+		});
+        
+	}
+	
+	@Test
+	public void deleteById_Sucesso_BuscarUmRecursoExistente() throws JsonProcessingException {
+        			
+		Orcamento orc1 = new Orcamento(null, null, null, null, null, false);
+		orc1.setId(1L);
+		
+		when(this.repository.save(orc1)).thenReturn(orc1);
+		
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		String json = ow.writeValueAsString(orc1);
+				
+		json = json.replaceAll("\"" + "id" + "\"[ ]*:[^,}\\]]*[,]?", "");
+						
+		given().contentType(ContentType.JSON).when().delete("/orcamentos/{id}", 1L).andReturn().then().statusCode(HttpStatus.NO_CONTENT.value());
+			
+		verify(repository, times(1)).deleteById(1L);
+	    verifyNoMoreInteractions(repository);        
+	}
+
 
 }
